b7ff925f2eacfdf85ed1528b199d29ab922e2ce3
darshan1472 <darshanchavan1472@gmail.com>
2024-03-28 19:45:07 +0530

    commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about-section'>
+    <h1 className='section-title'>About us</h1>
+    <p>One weekend Liz and I took a short break from life as lawyers and decided to dream a little. We brainstormed, reminisced, reflected and projected, all with a goal of discovering where our dreams intersected. It was clear after a while that our life change would involve hospitality.
+
+We love people and their stories, and nothing brings people together like a great meal in a memorable setting. Our first restaurant [ad]venture was FARM, also located in downtown Palm Springs. FARM allowed us to bring our love of the French countryside to a charming little courtyard, chock full of flowers and tucked away from the hustle and bustle. We love everything that has become FARM, but there was more to be said.So part of our life story involves food - but it also includes travel, history, and of course, tequila! Tac/Quila is our made up word for combining gourmet Jalisco style cuisine with specialty tequilas and mezcals, in an effort to transport our guests south of the border and into a culture rich in flavor, color, art, and authenticity.
+
+At the same time Palm Springs was laying the foundation for its current international reputation as a mid-century modern mecca, Mexico City was producing and welcoming some of the most important artists of that same movement. From renowned painters like Diego Rivera and Rufino Tamayo to mid century furniture and accessory artisans Pepe Mendoza and Arturo Pani, Mexico was sinking its cultural roots and solidifying its position as a leader of classic design.</p>
+   </section>
+  )
+}
+
+export default About
